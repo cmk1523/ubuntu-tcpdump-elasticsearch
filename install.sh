7642474e2d6842d4f3ad8a21246833684ac4f52d
cmk1523@gmail.com <cmk1523@gmail.com>
2017-08-02 01:44:16 -0400

    na
    na

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install tshark
 
-#sudo mergecap -w merge.pcap -F pcap tcp.pcap*
+#sudo mergecap -w merge.pcap -F pcap tcpdump.pcap*
 #sudo ivstools --convert merge.pcap merge.ivs
-#wpaclean merge.handshakes merge.pcap
+#wpaclean merge.handshakes2 merge.pcap
 # OR...
 #tshark -r merge.pcap -2R "eapol || wlan.fc.type_subtype == 0x08" -w merge.handshakes
